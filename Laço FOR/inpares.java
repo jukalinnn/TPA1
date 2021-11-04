e85b84c00497b6fe3d9afaf89eacf3cf7f090c29
jukalinnn <92936926+jukalinnn@users.noreply.github.com>
2021-11-03 22:25:28 -0300

    Add files via upload

---
+++ Laço FOR/inpares.java
@@ -0,0 +1,15 @@
+
+public class Impares {
+	public static void main (String args[]) {
+		
+		for (int i = 100; i < 201; i++) {
+			 
+            if ((i % 2) != 0) {
+                System.out.println("Os números ímpares que ficam entre 100 e 200 são: " + i);
+            }
+ 
+        }
+ 
+    }
+ 
+}
